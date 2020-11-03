163237b7713a6c76deb9624da061c7373e93af3a
crazylimesucide <72753034+crazylimesucide@users.noreply.github.com>
2020-11-03 15:01:47 +0200

    Add files via upload

---
+++ Zahist.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    float a, b, d;
+    string c;
+    int p = 1;
+    d = 0;
+
+    while (p = 1)
+    {
+        cout << "Chislo a = "; cin >> a; 
+        cout << "Diya = "; cin >> c;
+        cout << "Chilslo b = "; cin >> b;
+        if (c == "+")
+            d = a + b;
+        else
+         {
+            if(c == "-")
+                d = a - b;
+                else
+               
+                if (c == "*")
+                    d = a * b;
+               
+                else
+                    if (c == "/")
+                        d = a / b;                           
+        }
+        cout << "Result = " << d << endl << "Shob prodovjiti natisnit 1";
+        p = 0;
+        cin >> p;
+    }
+}
